083e6df4fe69620ff020b760cb3956b4e954c0be
Duc Tram <tramduc1996@yahoo.com>
2019-01-28 16:40:55 -0800

    Updated for client and server files

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello</div>;
+    return (
+      <React.Fragment>
+        <h1>This is an example</h1>
+      </React.Fragment>
+    );
   }
 }
 
